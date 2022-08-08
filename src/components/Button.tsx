6c3d608837d44c540eca060bd14a8afdb31fc0ea
olga <gatalolia86@gmail.com>
2022-08-08 13:49:22 +0200

    getRequest+Button

---
+++ src/components/Button.tsx
@@ -0,0 +1,14 @@
+type ButtonTypeProps = {
+    nickName: string
+    callBack: () => void
+}
+
+
+export const Button = (props:ButtonTypeProps) => {
+    const onClickButtonHandler = () => {
+      props.callBack()
+    }
+  return (
+      <button onClick={onClickButtonHandler}>{props.nickName}</button>
+  )
+}
